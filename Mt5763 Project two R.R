759d207bcb60378d2906216a5995c53b8fddd5d9
Euan Sunderland <ecs7@st-andrews.ac.uk>
2018-10-23 16:39:19 +0100

    Initial Commit

---
+++ Mt5763 Project two R.R
@@ -0,0 +1,68 @@
+#Generating some random data to work with.
+set.seed(1)
+n <- 100
+x <- runif(n)
+y <- x + 1 + rexp(n)
+
+
+#Data for use with vector needs to be ordered: Intercept, covariates, dependent variable.
+data.m <- cbind("(Intercept)" = 1, x, y)
+data.f <- data.frame(x,y)
+
+
+#~~~~~~~~~~Vectorisation~~~~~~~~~~~~~~~~~~~~~~~~~
+
+boot.lm.vector <- function(inputData) {
+  
+  #Random sampling from input data with replacement.
+  d <- inputData[sample.int(nrow(inputData), replace = T),]
+  
+  #Define matrix with covariates and intercept.
+  a <- ncol(inputData)-1
+  X <- d[, 1:a]
+  
+  #Vector with dependent variable.
+  y <- d[, a+1]
+  
+  #Solve for coefficients.
+  solve(crossprod(X), crossprod(X,y))
+}
+
+system.time(r1 <- t(replicate(10000, boot.lm.vector(data.m))[,1,]))
+
+
+#~~~~~~~~~~~~~Carl's function~~~~~~~~~~~~~~~~~~~~~~
+
+lmBoot <- function(inputData, nBoot){
+  
+  for(i in 1:nBoot){
+    
+    # resample our data with replacement
+    bootData <- inputData[sample(1:nrow(inputData), nrow(inputData), replace = T),]
+    
+    # fit the model under this alternative reality
+    bootLM <- lm(y ~ x, data = bootData)
+    
+    # store the coefs
+    if(i == 1){
+      
+      bootResults <- matrix(coef(bootLM), ncol = 2)
+      
+    } else {
+      
+      bootResults<- rbind(bootResults, matrix(coef(bootLM), ncol = 2))
+      
+    }
+    
+    
+  } # end of i loop
+  
+  bootResults
+  
+}
+
+system.time(r2 <- lmBoot(data.f, 10000))
+
+
+#Need to check results are the same!
+
